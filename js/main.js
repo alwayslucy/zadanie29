66d2338ae32882b8c80b0829314f06837cf3a964
lkipland <lucyna.gamon@gmail.com>
2021-05-27 12:06:40 +0200

    pierwszy commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let firstNumber = 4;
+
+let secondNumber = 8;
+
+function sum (par1,par2) { 
+    
+    let result = par1 + par2;
+
+    return result;
+}
+
+console.log (sum(firstNumber,secondNumber));
